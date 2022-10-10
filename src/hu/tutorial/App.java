200f609fc2a46531ecacccfb0d445671984bdebf
pfazekas <v-pfazekas@hotels.com>
2022-10-10 08:34:23 +0200

    Add summation()

--- src/hu/tutorial/App.java
+++ src/hu/tutorial/App.java
@@ -8,14 +8,32 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("Progrmozási tételek megvalósítása");
+        numbers = init();
+        print();
+        System.out.println("A sorozat összege: " + summation());
     }
 
     private static int[] init() {
         int[] nums = new int[100];
         Random random = new Random();
         for (int i = 0; i < 100; i++) {
-            nums[i] = random.nextInt(99);
+            nums[i] = random.nextInt(99) + 1;
         }
         return nums;
+    }
+
+    private static void print() {
+        for (int i = 0; i < 100; i++) {
+            System.out.printf("%4d", numbers[i]);
+        }
+        System.out.println();
+    }
+
+    private static int summation() {
+        int sum = 0;
+        for (int i = 0; i < 100; i++) {
+            sum = sum + numbers[i];
+        }
+        return sum;
     }
 }
