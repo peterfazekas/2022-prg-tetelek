db9b82ead9137d74c9e4be9dddee55b21d7e5bad
pfazekas <v-pfazekas@hotels.com>
2022-10-10 09:24:20 +0200

    Add selection()

--- src/hu/tutorial/App.java
+++ src/hu/tutorial/App.java
@@ -11,19 +11,27 @@ public class App {
         numbers = init();
         print();
         System.out.println("A sorozat összege: " + summation());
+        int divisor = 3;
+        boolean isExist = decision(divisor);
+        System.out.println("A sorozatban " + (isExist ? "van " : "nincs ") +
+                divisor + "-al osztható szám.");
+        if (isExist) {
+            System.out.println("A sorozat " + (selection(divisor) + 1) +
+                    ". eleme osztható " + divisor + "-val.");
+        }
     }
 
     private static int[] init() {
         int[] nums = new int[100];
         Random random = new Random();
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < nums.length; i++) {
             nums[i] = random.nextInt(99) + 1;
         }
         return nums;
     }
 
     private static void print() {
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < numbers.length; i++) {
             System.out.printf("%4d", numbers[i]);
         }
         System.out.println();
@@ -31,9 +39,25 @@ public class App {
 
     private static int summation() {
         int sum = 0;
-        for (int i = 0; i < 100; i++) {
-            sum = sum + numbers[i];
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];  // sum = sum + numbers[i];
         }
         return sum;
+    }
+
+    private static boolean decision(int divisor) {
+        int i = 0;
+        while (i < numbers.length && !(numbers[i] % divisor == 0)) {
+            i++;
+        }
+        return i < numbers.length;
+    }
+
+    private static int selection(int divisor) {
+        int i = 0;
+        while (!(numbers[i] % divisor == 0)) {
+            i++;
+        }
+        return i;
     }
 }
