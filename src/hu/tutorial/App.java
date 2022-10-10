16afb227c07d479afa98961c8e5b16b0c8bb8348
pfazekas <v-pfazekas@hotels.com>
2022-10-10 08:08:05 +0200

    Add init()

--- src/hu/tutorial/App.java
+++ src/hu/tutorial/App.java
@@ -1,8 +1,21 @@
 package hu.tutorial;
 
+import java.util.Random;
+
 public class App {
+
+    private static int[] numbers;
 
     public static void main(String[] args) {
         System.out.println("Progrmozási tételek megvalósítása");
+    }
+
+    private static int[] init() {
+        int[] nums = new int[100];
+        Random random = new Random();
+        for (int i = 0; i < 100; i++) {
+            nums[i] = random.nextInt(99);
+        }
+        return nums;
     }
 }
